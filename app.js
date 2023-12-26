ca55522f15b2f02600092c3773f6271996b7d4ce
Arjita Arora <arjita.arora10@gmail.com>
2023-12-26 09:21:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function submitForm() {
+    // You can add your form submission logic here
+    alert('Form submitted successfully!');
+}
+function socialSignUp(provider) {
+    alert(`Signing up with ${provider}.`);
+    // Add your social sign-up logic here
+}
